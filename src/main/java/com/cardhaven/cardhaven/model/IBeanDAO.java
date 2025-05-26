d3116ec838da8a166d8a180de59a7b97fe657b81
Andrea Squitieri <andrea.squitieri@disroot.org>
2025-05-26 11:57:35 +0200

    Added DAO interface

---
+++ src/main/java/com/cardhaven/cardhaven/model/IBeanDAO.java
@@ -0,0 +1,14 @@
+package com.cardhaven.cardhaven.model;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+public interface IBeanDAO<T> {
+    void doSave(T t) throws SQLException;
+
+    boolean doDelete(T t) throws SQLException;
+
+    T doRetrieveByKey(int code) throws SQLException;
+
+    Collection<T> doRetrieveAll(String order) throws SQLException;
+}
