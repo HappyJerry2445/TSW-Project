e61c1b8611f6bfa9a05336a35e575d2a4bb62f9f
Andrea Squitieri <andrea.squitieri@disroot.org>
2025-05-27 12:01:27 +0200

    Edited the bean to contain the key

--- src/main/java/com/cardhaven/cardhaven/model/beans/ProductCategory.java
+++ src/main/java/com/cardhaven/cardhaven/model/beans/ProductCategory.java
@@ -5,52 +5,100 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class ProductCategory implements Serializable {
-    private int productId;
-    private int categoryId;
+    private ProductCategoryKey key;
+
+    public ProductCategory(int productId, int categoryId) {
+        this.key = new ProductCategoryKey(productId, categoryId);
+    }
 
     public ProductCategory() {
     }
 
-    public ProductCategory(int productId, int categoryId) {
-        this.productId = productId;
-        this.categoryId = categoryId;
-    }
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
 
-    // Getters and Setters
-    public int getProductId() {
-        return productId;
-    }
-
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
-
-    public int getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
+        ProductCategory that = (ProductCategory) o;
+        return Objects.equals(key, that.key);
     }
 
     @Override
     public String toString() {
         return "ProductCategory{" +
-                "productId=" + productId +
-                ", categoryId=" + categoryId +
+                "key=" + key +
                 '}';
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ProductCategory that = (ProductCategory) o;
-        return productId == that.productId && categoryId == that.categoryId;
+    public int hashCode() {
+        return Objects.hash(key);
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(productId, categoryId);
+
+    public int getProductId() {
+        return key.getProductId();
+    }
+
+    public void setProductId(int productId) {
+        key.setProductId(productId);
+    }
+
+    public int getCategoryId() {
+        return key.getCategoryId();
+    }
+
+    public void setCategoryId(int categoryId) {
+        key.setCategoryId(categoryId);
+    }
+
+
+    static public class ProductCategoryKey implements Serializable {
+        private int productId;
+        private int categoryId;
+
+        public ProductCategoryKey(int productId, int categoryId) {
+            this.productId = productId;
+            this.categoryId = categoryId;
+        }
+
+        public ProductCategoryKey() {
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+
+            ProductCategoryKey that = (ProductCategoryKey) o;
+            return productId == that.productId && categoryId == that.categoryId;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(productId, categoryId);
+        }
+
+        @Override
+        public String toString() {
+            return "ProductCategoryKey{" +
+                    "productId=" + productId +
+                    ", categoryId=" + categoryId +
+                    '}';
+        }
+
+        public int getProductId() {
+            return productId;
+        }
+
+        public void setProductId(int productId) {
+            this.productId = productId;
+        }
+
+        public int getCategoryId() {
+            return categoryId;
+        }
+
+        public void setCategoryId(int categoryId) {
+            this.categoryId = categoryId;
+        }
     }
 }
