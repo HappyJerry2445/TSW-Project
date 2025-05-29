e813fb3238f0e9bbf7a2635a8b94d5a1f8720f50
Andrea_Stefanile <andrea.stefanile@outlook.com>
2025-05-29 22:05:31 +0200

    add Review beans

---
+++ src/main/java/com/cardhaven/cardhaven/model/beans/Review.java
@@ -0,0 +1,130 @@
+package com.cardhaven.cardhaven.model.beans;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Review implements Serializable {
+
+	private int reviewId;
+	private int productId;
+	private int userId;
+	private int rating;
+	private String title;
+	private String reviewText;
+	private LocalDateTime createdAt;
+	private ReviewStatus reviewStatus;
+
+
+	public Review() {}
+
+	public Review(int reviewId, int productId, int userId, int rating, String title, String reviewText, LocalDateTime createdAt, ReviewStatus reviewStatus) {
+		this.reviewId = reviewId;
+		this.productId = productId;
+		this.userId = userId;
+		this.rating = rating;
+		this.title = title;
+		this.reviewText = reviewText;
+		this.createdAt = createdAt;
+		this.reviewStatus = reviewStatus;
+	}
+
+	public int getReviewId() {
+		return reviewId;
+	}
+
+	public void setReviewId(int reviewId) {
+		this.reviewId = reviewId;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getReviewText() {
+		return reviewText;
+	}
+
+	public void setReviewText(String reviewText) {
+		this.reviewText = reviewText;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public ReviewStatus getReviewStatus() {
+		return reviewStatus;
+	}
+
+	public void setReviewStatus(ReviewStatus reviewStatus) {
+		this.reviewStatus = reviewStatus;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (object == null || getClass() != object.getClass()) return false;
+
+		Review review = (Review) object;
+		return reviewId == review.reviewId && productId == review.productId && userId == review.userId && rating == review.rating && Objects.equals(title, review.title) && Objects.equals(reviewText, review.reviewText) && Objects.equals(createdAt, review.createdAt) && reviewStatus == review.reviewStatus;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(reviewId, productId, userId, rating, title, reviewText, createdAt, reviewStatus);
+	}
+
+	@Override
+	public String toString() {
+		return "Review{" +
+				"reviewId=" + reviewId + '\'' +
+				", productId=" + productId + '\''+
+				", userId=" + userId + '\'' +
+				", rating=" + rating + '\''+
+				", title='" + title + '\'' +
+				", reviewText='" + reviewText + '\'' +
+				", createdAt=" + createdAt + '\''+
+				", reviewStatus=" + reviewStatus + '\''+
+				'}';
+	}
+
+	public enum ReviewStatus{
+		Pending,
+		Approved,
+		Rejected
+	}
+
+
+}
