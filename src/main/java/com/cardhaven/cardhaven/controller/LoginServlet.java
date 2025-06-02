9bda65475d3fb34539239e103e294c23e60872c6
Gennaro Carmine Tozza <gennarocarmine@gmail.com>
2025-06-02 17:28:54 +0200

    create LoginServlet.java

---
+++ src/main/java/com/cardhaven/cardhaven/controller/LoginServlet.java
@@ -0,0 +1,42 @@
+package com.cardhaven.cardhaven.controller;
+
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/common/Login")
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        List<String> errors = new ArrayList<>();
+        RequestDispatcher dispatcherToLoginPage = request.getRequestDispatcher("login.jsp");
+
+        if (email == null || email.trim().isEmpty()) {
+            errors.add("Email cannot be empty");
+        }
+        if (password == null || password.trim().isEmpty()) {
+            errors.add("Password cannot be empty");
+        }
+        if (!errors.isEmpty()) {
+            request.setAttribute("errors", errors);
+            dispatcherToLoginPage.forward(request, response);
+            return;
+        }
+
+        email = email.trim();
+        password = password.trim();
+
+        //TODO
+    }
+}
