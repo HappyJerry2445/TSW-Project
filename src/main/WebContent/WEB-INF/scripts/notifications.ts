57fbcba107f3538a9a89d1fc1476dc55d78b8974
Andrea Squitieri <andrea.squitieri@disroot.org>
2025-06-04 16:17:58 +0200

    Created structure for notification system

---
+++ src/main/WebContent/WEB-INF/scripts/notifications.ts
@@ -0,0 +1,46 @@
+(function () {
+    const notificationContainer = document.getElementById('notification-container');
+
+    function createNotificationElement(message: string, type: string) {
+        const notification = document.createElement('div');
+        notification.classList.add('notifcation', type);
+        notification.innerHTML = `
+            <span>${message}</span>
+            <span class="notification-close">&times;</span>
+        `;
+        return notification;
+    }
+
+    function showNotification(message: string, type: string = 'info', duration: number = 5000) {
+        const notification = createNotificationElement(message, type);
+        notificationContainer.prepend(notification);
+
+        requestAnimationFrame(() => {
+            notification.classList.add('show');
+        });
+
+        if (duration > 0) {
+            setTimeout(() => {
+                hideNotification(notification);
+            }, duration);
+        }
+
+        notification.querySelector('.notification-close').addEventListener('click', () => {
+            hideNotification(notification);
+        });
+    }
+
+    function hideNotification(notificationElement: HTMLDivElement) {
+        notificationElement.classList.remove('show');
+        notificationElement.addEventListener('transitionend', () => {
+            notificationElement.remove();
+        }, {once: true});
+    }
+
+    window["notify"] = {
+        success: (message, duration) => showNotification(message, 'success', duration),
+        error: (message, duration) => showNotification(message, 'error', duration),
+        info: (message, duration) => showNotification(message, 'info', duration),
+        warning: (message, duration) => showNotification(message, 'warning', duration),
+    };
+})
--- src/main/WebContent/components/header.jsp
+++ src/main/WebContent/components/header.jsp
@@ -1,6 +1,6 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-
+<div id="notification-container"></div>
 <header>
     <div class="container">
         <a href="/" class="logo">CardHaven</a>
