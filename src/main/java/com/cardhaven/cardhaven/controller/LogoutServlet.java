9faeddb0e2e3ca42ef1b356eab2c6d70adb44c77
Andrea Squitieri <andrea.squitieri@disroot.org>
2025-06-04 14:51:44 +0200

    Created logout servlet

---
+++ src/main/java/com/cardhaven/cardhaven/controller/LogoutServlet.java
@@ -0,0 +1,33 @@
+package com.cardhaven.cardhaven.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processLogout(req, resp);
+
+    }
+
+    private void processLogout(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        resp.sendRedirect(req.getContextPath() + "/");
+    }
+}
