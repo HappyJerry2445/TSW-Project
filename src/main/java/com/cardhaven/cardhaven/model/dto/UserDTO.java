67107f0ae62afc828a765f6a0def05ab51a6f382
Andrea Squitieri <andrea.squitieri@disroot.org>
2025-06-05 10:32:50 +0200

    Created new User constructor

--- src/main/java/com/cardhaven/cardhaven/model/dto/UserDTO.java
+++ src/main/java/com/cardhaven/cardhaven/model/dto/UserDTO.java
@@ -28,6 +28,17 @@ public class UserDTO implements Serializable {
         this.role = role;
     }
 
+    public UserDTO(String firstName, String lastName, String email, String passwordHash) {
+        this.id = 0;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.createdAt = LocalDateTime.now();
+        this.lastLogin = LocalDateTime.now();
+        this.role = Role.Customer;
+    }
+
     public int getId() {
         return id;
     }
