558d9d6aa2bf3c7b84943b6ac6fe3bf1eac7c5c6
Andrea Squitieri <andrea.squitieri@disroot.org>
2025-06-08 15:43:19 +0200

    Initialized ChangePasswordServlet

---
+++ src/main/java/com/cardhaven/cardhaven/controller/ChangePasswordServlet.java
@@ -0,0 +1,8 @@
+package com.cardhaven.cardhaven.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/common/change-password")
+public class ChangePasswordServlet extends HttpServlet {
+}
