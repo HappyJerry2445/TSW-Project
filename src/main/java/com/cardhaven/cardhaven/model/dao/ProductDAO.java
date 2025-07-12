072f842ebee5736d2a7d9201d8828ae56738e784
Andrea_Stefanile <andrea.stefanile@outlook.com>
2025-07-12 19:27:27 +0200

    addedd getProductsByCategory in ProductDAO

--- src/main/java/com/cardhaven/cardhaven/model/dao/ProductDAO.java
+++ src/main/java/com/cardhaven/cardhaven/model/dao/ProductDAO.java
@@ -98,27 +98,7 @@ public class ProductDAO implements GenericDAO<ProductDTO, Integer> {
         return productDTO;
     }
 
-    public Collection<ProductDTO> getProductsByCategory (int categoryId) throws SQLException{
-        Collection<ProductDTO> products = new ArrayList<>();
-        String sql = """
-                SELECT *
-                FROM Product
-                JOIN ProductCategory ON Product.ProductId = ProductCategory.ProductID
-                WHERE ProductCategory.ProductID = ? AND Product.IsActive = true
-                ORDER BY Product.ProductName
-                """;
-        try (Connection conn = dataSource.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)){
-            ps.setInt(1, categoryId);
 
-            try (ResultSet rs = ps.executeQuery()){
-                while (rs.next()){
-                    ProductDTO product= extractProductFromResultSet(rs);
-                    products.add(product);
-                }
-            }
-        }
-        return products;
-    }
 
     public Collection<ProductDTO> getAll(String order) throws SQLException {
         Collection<ProductDTO> productDTOS = new ArrayList<>();
@@ -198,5 +178,27 @@ public class ProductDAO implements GenericDAO<ProductDTO, Integer> {
             throw new IllegalArgumentException("CreatedAt timestamp cannot be null.");
         }
 
+    }
+
+    public Collection<ProductDTO> getProductsByCategory (int categoryId) throws SQLException{
+        Collection<ProductDTO> products = new ArrayList<>();
+        String sql = """
+                SELECT *
+                FROM Product
+                JOIN ProductCategory ON Product.ProductId = ProductCategory.ProductID
+                WHERE ProductCategory.ProductID = ? AND Product.IsActive = true
+                ORDER BY Product.ProductName
+                """;
+        try (Connection conn = dataSource.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setInt(1, categoryId);
+
+            try (ResultSet rs = ps.executeQuery()){
+                while (rs.next()){
+                    ProductDTO product= extractProductFromResultSet(rs);
+                    products.add(product);
+                }
+            }
+        }
+        return products;
     }
 }
