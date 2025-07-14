bc0e27e94a9c7159f6b716329e74a3a51a9b651f
Gennaro Carmine Tozza <gennarocarmine@gmail.com>
2025-07-14 22:16:22 +0200

    Create ConfirmServlet

---
+++ src/main/java/com/cardhaven/cardhaven/controller/ConfirmServlet.java
@@ -0,0 +1,10 @@
+package com.cardhaven.cardhaven.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/common/checkout/confirm")
+public class ConfirmServlet extends HttpServlet {
+    //TODO
+
+}
